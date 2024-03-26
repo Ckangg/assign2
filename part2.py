6566372e0b9ffdc78786dfe78ed7eeef48abca7a
Ckangg <141784836+Ckangg@users.noreply.github.com>
2024-03-26 17:31:00 -0400

    Update part2.py

--- part2.py
+++ part2.py
@@ -58,13 +58,13 @@ def compute():
     random_state = 12
 
     # Generating the dataset
-    a = make_blobs(n_samples=n_samples, centers=centers, center_box=center_box, random_state=random_state,return_centers=True)
+    a,b,c = make_blobs(n_samples=n_samples, centers=centers, center_box=center_box, random_state=random_state,return_centers=True)
 
     # Displaying the generated data and labels
   
 
     # dct: return value from the make_blobs function in sklearn, expressed as a list of three numpy arrays
-    dct = answers["2A: blob"] = [a[0],a[1],a[2]]
+    dct = answers["2A: blob"] = [a,b,c]
 
     """
     B. Modify the fit_kmeans function to return the SSE (see Equations 8.1 and 8.2 in the book).
